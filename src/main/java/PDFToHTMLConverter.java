0d1099f78683858e44a4d6c2b069a34d2fb80173
Letenyei Ádám <letenyeidam89@gmail.com>
2020-11-05 20:49:38 +0100

    Getting started

---
+++ src/main/java/PDFToHTMLConverter.java
@@ -0,0 +1,23 @@
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.fit.pdfdom.PDFDomTree;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
+public class PDFToHTMLConverter {
+    public static void generateHTMLFromPDF(String filename) {
+        try {
+            PDDocument pdf = PDDocument.load(new File(filename));
+            Writer output = new PrintWriter("D:\\converted\\converted.pdf","UTF-8");
+            new PDFDomTree().writeText(pdf,output);
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+    }
+}
